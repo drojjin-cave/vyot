f170f71f843c5219e2533a315ba7811b13e0fe2d
drojjin <drojjin@univers.su>
2025-02-27 19:03:12 +0700

    тест 14

--- core/handlers/callback.py
+++ core/handlers/callback.py
@@ -33,7 +33,7 @@ async def select_ruls(call: CallbackQuery, bot: Bot):
 
 async def select_profile(call: CallbackQuery, bot: Bot):
 
-    await call.message.answer('тест епт')
+    await call.message.answer('тест епт1')
     #await bot.edit_message_reply_markup(chat_id=call.from_user.id, message_id=call.message.message_id, reply_markup=None)
 
     await call.answer()
