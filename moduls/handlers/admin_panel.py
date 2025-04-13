2b9e260e8888c9247a300a301ba9e3bbfeaa2984
Admin <drojjin@univers.su>
2025-04-14 01:00:39 +0700

    Ловим ошибки часть 1

--- moduls/handlers/admin_panel.py
+++ moduls/handlers/admin_panel.py
@@ -1,7 +1,12 @@
-from aiogram import Bot, Router
-from aiogram.types import CallbackQuery, FSInputFile, Message
+import traceback
+from inspect import Traceback
+
+from aiogram import Bot, Router, F
+from aiogram.enums import ParseMode
+from aiogram.types import CallbackQuery, FSInputFile, Message, ErrorEvent
 from moduls.keyboards.admin_panel_keyboard import admin_menu_main_keyboard
-from aiogram.filters import Command
+from aiogram.filters import Command, ExceptionTypeFilter
+from aiogram.handlers import ErrorHandler
 
 from moduls.settings import settings
 from moduls.utils.static_text import MAIN_PHOTO_PATH
@@ -11,6 +16,7 @@ ADMINS_VPN = settings.bots.admins_vpn
 
 
 admin_handlers = Router(name=__name__)
+admin_chanel = "-1002636005700"
 
 @admin_handlers.message(Command("logs"))
 async def send_logs(message: Message, bot: Bot, n=30):
@@ -25,7 +31,7 @@ async def send_logs(message: Message, bot: Bot, n=30):
     log_local = r'C:\Users\Admin\Desktop\Документы\!BOTS\vyot-bot\vyot\tg-vyot-error.log'
     log_local_out = f'C:\\Users\\Admin\\Desktop\Документы\\!BOTS\\vyot-bot\\vyot\\logs\\{date_update_info}.log'
 
-    admin_chanel = "-1002636005700"
+
 
     await message.delete()
     if message.from_user.id in ADMINS_VPN:
@@ -37,6 +43,22 @@ async def send_logs(message: Message, bot: Bot, n=30):
         await bot.send_document(admin_chanel, document=FSInputFile(path=log_out),
                                 caption=f'Логи по запросу <b>{message.from_user.username}</b>')
 
+
+@admin_handlers.error()
+async def error_handler(event: ErrorEvent, bot: Bot):
+
+
+    date_update_info = datetime.now(timezone.utc)
+    date_update_info = (date_update_info + timedelta(hours=7, minutes=0)).strftime('%d.%m.%Y %H:%M:%S')
+
+    text = (f'Возникла ошибка!\n'
+            f'Время - <b>{date_update_info}</b>\n\n'
+            f'<b>Ошибка:</b>\n'
+            f'<blockquote>{event.exception}</blockquote>'
+            )
+
+    await bot.send_message(admin_chanel, text=text)
+
 async def get_active_users(call: CallbackQuery, bot: Bot):
     '''Активные пользователи'''
 
@@ -44,7 +66,6 @@ async def get_active_users(call: CallbackQuery, bot: Bot):
 
 
     await call.answer()
-
 
 async def back_from_admin(call: CallbackQuery, bot: Bot):
 
